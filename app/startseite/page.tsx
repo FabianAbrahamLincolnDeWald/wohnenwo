b8e3a5c2148250e7aefb88f60b33097d76c55a6a
Fabian Dewald <fabiandewald@MacBook-Pro-von-Fabian.local>
2025-11-18 18:44:47 +0100

    fix import FundamentKarten

--- app/startseite/page.tsx
+++ app/startseite/page.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import HeroSection from "@/components/sektionen/HeroSection";
 import PanelImageSection from "@/components/sektionen/PanelImageSection";
 import InterstitialTextSection from "@/components/sektionen/InterstitialTextSection";
-import FundamentKarten from "@/components/sektionen/FundamentKarten.tsx";
+import FundamentKarten from "@/components/sektionen/FundamentKarten";
 
 export default function HeroShell() {
   return (
